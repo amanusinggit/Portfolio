99ab4d4ba064b637cbeb9ee498d1e33b1394584c
amanusinggit <singh.aman.2016620@gmail.com>
2026-05-01 17:53:54 +0530

    Header created

--- index.jsx
+++ index.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Profile from './Components/Profile.jsx';
+import Skills from './Components/Skills.jsx';
+import Socials from './Components/Socials.jsx';
+import Projects from './Components/Projects.jsx';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -9,6 +13,24 @@ const routeConfig = createBrowserRouter([
     {
         path: '/',
         element: <App/>, 
+        children: [
+            {
+                path: "profile",
+                element: <Profile />
+            },
+            {
+                path: "skills",
+                element: <Skills />
+            },
+            {
+                path: "socials",
+                element: <Socials />
+            },
+            {
+                path: "projects",
+                element: <Projects />
+            }
+        ]
     },
 ]);
 
---
+++ tab-data.js
@@ -0,0 +1,22 @@
+export const tabs = [
+    {
+        icon: "fa-regular fa-user",
+        label: "Profile",
+        to: "/profile"
+    },
+    {
+        icon: "fa-regular fa-folder-open",
+        label: "Projects",
+        to: "/projects"
+    },
+    {
+        icon: "fa-solid fa-screwdriver-wrench",
+        label: "Skills",
+        to: "/skills"
+    },
+    {
+        icon: "fa-solid fa-hashtag",
+        label: "Socials",
+        to: "/socials"
+    },
+];
